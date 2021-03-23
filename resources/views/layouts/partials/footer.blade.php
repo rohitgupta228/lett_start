4ffbab092243eee8fc07775291cc77b66f736354
Gagan <ggbbhatia123@gmail.com>
2021-03-23 23:17:35 +0530

    chat bot code

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -88,6 +88,21 @@
       });
     })
 </script>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        setTimeout(function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/6058a68ff7ce18270932a9fd/1f1d4l8uu';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        }, 1000)
+    })();
+</script>
+<!--End of Tawk.to Script-->
 @yield('footer_script')
 <!-- Javascript's -->
 </body>
