f21f42298dbcbf9e7b5cd94534f010f48cc7d8c8
Gagan <ggbbhatia123@gmail.com>
2021-05-06 22:54:53 +0530

    bootstrap template content

---
+++ resources/views/layouts/partials/bootstrap-templates.blade.php
@@ -0,0 +1,161 @@
+<section class="section bg-light">
+  <div class="container">
+    <h2 class="mb-30">Why Choose Premium Bootstrap Templates for Website Development?</h2>
+    <p>Premium bootstrap templates are designed for easy use. It mitigates the compatibility issue on a wide range of
+      devices. Bootstrap technology allows the website to be responsive.</p>
+    <p>A single design is compatible with all the modern devices and screen sizes available in the markets.
+      Additionally, bootstrap enhances the user experience.</p>
+    <div id="read-more-1" style="display: none;">
+      <p>Speed, easy browsing, fast development, quick functional changes, freedom to customize code, and open-source
+        platforms are key highlights of the bootstrap template. Users get to enjoy several benefits while obtaining the
+        bootstrap technology.</p>
+      <p>Moreover, the future expansion of the website layout is possible without needing to change the base structure.
+        Small changes to the core files would be sufficient to apply the required changes to the whole website.</p>
+      <p>Due to the flexibility that bootstraps, many top companies trust the <a
+          href="https://lettstartdesign.com/category/bootstrap-templates">bootstrap templates</a> for their corporate
+        websites. The technology enables you to make a stellar website with the free bootstrap template. These templates
+        would give you exclusivity and make your website stand out in the crowd.</p>
+    </div>
+    <div class="mt-30">
+      <a href="javascript:void(0)" class="btn btn-primary-gred read-more-link" data-href="#read-more-1">
+        Read More
+      </a>
+    </div>
+  </div>
+</section>
+<section class="section">
+  <div class="container">
+    <div class="row flex-lg-row-reverse align-items-center">
+      <div class="col-lg-8 pl-lg-5">
+        <h3 class="mb-30">Amplify Scalability and Compatibility</h3>
+        <p>Bootstrap themes promote mobile-first technology. The single website would be compatible with desktop, mobile
+          devices, and tablets. Use premium bootstrap templates for building your website with ease.</p>
+        <p>Install the theme on the server, and the site is live for the users. If you are not willing to spend money
+          upfront, you can select free premium bootstrap themes. These themes are offered free of cost with no
+          restraint.</p>
+        <p>The scalability of the existing digital infrastructure is a daunting task. The bootstrap simplifies the
+          process with the help of the futuristic coding framework that facilitates the programmer to extend the number
+          of pages or features without needing to change the site's core functionality.</p>
+      </div>
+      <div class="col-lg-4">
+        <img src="{{ url('assets/images/bootstrap-main.png') }}" class="img-fluid" alt="" />
+      </div>
+    </div>
+  </div>
+</section>
+<section class="section bg-light">
+  <div class="container">
+    <h3 class="mb-30">Benefits of Using Premium Bootstrap Templates</h3>
+    <p>This template come with more features and customization functionality. Compared with the free template, they come
+      with limited resources and the technology used in the development of the themes.</p>
+    <p>You get premium support when you buy the premium bootstrap theme. Most importantly, the free templates are widely
+      used, which takes away the exclusivity factor of the website design.</p>
+    <div class="mt-30">
+      <div class="mb-30">
+        <div class="h5 mt-30">Easy to Use</div>
+        <p>Bootstrap templates are easy to use. No special skill requires installing the theme on the hosting server.
+          The premium theme comes with all the required resources, so you do not have to add or remove anything from the
+          template. It is a robust template design to engage the users.</p>
+      </div>
+      <div id="read-more-3" style="display: none;">
+        <div class="mb-30">
+          <div class="h5 mt-30">Call to Action</div>
+          <p>The call to action buttons such as "register now", "buy now", "join the community", "signup", "add to cart", "Register for the newsletter" should be easy to spot on the page. The use of the right contrast colors on the button would make it stand out in the content.</p>
+          <p>The ultimate goal of the landing page template is to increase the conversion. If none of the users reaches the page and no action is taken, then whole efforts are futile.</p>
+        </div>
+        <div class="mb-30">
+            <div class="h5 mt-30">Auto Adjust to the screen size</div>
+            <p>With the premium bootstrap templates, you do not have to worry about the browser or the device size of the end-users. The templates come with auto-adjusted and modification features that align the element precisely to offer a great experience to the user. All the themes developed by the LettStart Design fit perfectly to computer, mobile, and tablet screens. </p>
+        </div>
+        <div class="mb-30">
+            <div class="h5 mt-30">Customization</div>
+            <p>Website customization is an essential part of website design. As your business grows, you will need customization of the website element to match the latest trend. The LettStart Design offers the complete resources of the premium template that makes the customization of the website effortless.</p>
+            <p>Change the design anytime, add new features, add plug-in, social icons, modify the layout, use the custom hyperlinks, and add images. Many more facilities are offered with the premium admin template bootstrap.</p>
+        </div>
+        <div class="mb-30">
+            <div class="h5 mt-30">Instant development</div>
+            <p>The Bootstrap framework encourages the instant development of the website. No need for hard coding, use of third-party plug-in and applications to build the bootstrap website. All the templates are already made with the required resources and files.</p>
+            <p>If you are in a hurry to launch your business, the premium bootstrap templates are the one-stop solution for your need. The theme provides the foundation to start your new business or upgrade the existing business template to make your user enjoy the browsing of your content on the site.</p>
+        </div>
+        <div>
+            <div class="h5 mt-30">Cost-Effective</div>
+            <p><a href="https://lettstartdesign.com/">Bootstrap themes</a> come at affordable pricing. Comparing to the other technology, the bootstrap themes are required low investment. Also, you do not need a web designer or programmer to make changes to the site. So there will be no cost after you purchased the themes. A layperson with a basic understanding of HTML and CSS can change the template. It would be wise to buy the premium theme instead of wasting your time building the website from nothing.</p>
+        </div>
+      </div>
+      <div class="mt-45">
+        <a href="javascript:void(0)" class="btn btn-primary-gred read-more-link" data-href="#read-more-3">
+          Read More
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="section">
+  <div class="container">
+    <h3 class="mb-30">Drive More Customers With Landing Page Template</h3>
+    <p>Bootstrap landing page themes build by the Lettstart Design are readily available to make live on your website.
+      Bootstrap landing page download is quick. Download the theme, make it live on your server, make the desire
+      changes, and your landing page is ready to fly. You can find a wide range of custom templates for landing page the
+      Lettstart Design.</p>
+    <p>The website has a premium quality template at an affordable price. Use the demo section to view the template in
+      real-time. Every landing page is designed with modern technology and web development standards. More thought is
+      put into improving the user experience and reduce the efforts to browse the information. It is highly converting
+      landing pages found on the web.</p>
+  </div>
+</section>
+<section class="section bg-light">
+  <div class="container">
+    <h3 class="mb-30">Why Choose Lettstart Design?</h3>
+    <p>We are following modern technology with the state of the art design to offer a pleasant experience to the user.
+      We understand that it is essential if you want to win the heart of your customer. The bootstrap landing page
+      template developed by Lettstart Design offers many benefits to the user.</p>
+    <div id="read-more-2" style="display: none;">
+      <p>Several features are geared into the single page to reduce the efforts and time spent accessing the landing
+        page. The conversion rate of your marketing campaign would skyrocket when you start using the landing page
+        template bootstrap.</p>
+      <p>Browse from the hundreds of high-quality bootstrap landing page template listed on the Lettstart Design. Get
+        ready to use a template and start building your reputation on the web. For any query, you can reach the customer
+        support team of Lettstart Design.</p>
+      <p>The team will help you to select the right template suitable for your need. With 1-year customer support as
+        part of the package, you can implement the design without worrying about the technical aspect of the design.
+        Whenever you get stuck, contact the customer support team, and the problem will be resolved as quickly as
+        possible.
+      </p>
+    </div>
+    <div class="mt-45">
+      <a href="javascript:void(0)" class="btn btn-primary-gred read-more-link" data-href="#read-more-2">
+        Read More
+      </a>
+    </div>
+  </div>
+</section>
+<section class="section">
+  <div class="container">
+    <h3 class="mb-30">Frequently Asked Questions</h3>
+    <div class="accordion" id="accordionExample">
+      @foreach($faqList[$category] as $faq)
+
+      <div class="card active">
+        <div class="card-header" id="heading{{ $loop->index }}">
+          <a href="javascript:void(0)" class="accordion-toggle text-left" data-toggle="collapse"
+            data-target="#collapse{{ $loop->index }}" aria-expanded="true" aria-controls="collapse{{ $loop->index }}">
+            {!! $faq['question'] !!}
+          </a>
+        </div>
+
+        <div id="collapse{{ $loop->index }}" class="collapse show" aria-labelledby="heading{{ $loop->index }}">
+          <div class="card-body">
+            {!! $faq['answer'] !!}
+          </div>
+        </div>
+      </div>
+      @endforeach
+    </div>
+    <div class="mt-50">
+      <p><b>Other Bootstrap Templates : </b> <a href="https://lettstartdesign.com/category/landing-pages-templates">
+          Landing Page Templates </a> | <a
+          href="https://lettstartdesign.com/category/business-corporate-templates">Bootstrap Business Templates</a> | <a
+          href="https://lettstartdesign.com/category/angular-templates">Angular Template</a></p>
+    </div>
+  </div>
+</section>
--- resources/views/layouts/partials/landing-pages-templates.blade.php
+++ resources/views/layouts/partials/landing-pages-templates.blade.php
@@ -19,85 +19,63 @@
     <div class="container">
         <h3 class="mb-30">Bootstrap Landing Page Template Features</h3>
         <p>The bootstrap landing page template is high-quality pages of the website design to serve a single purpose. It is made to address the specific queries and provide the solution to the user's problem. Less clutter on the page with a step-by-step guide allows the user to understand the given information effortlessly. Comprehending the given information on the landing page is easy, which leads to instant conversion.</p>
-        <div class="responsive-tab mt-30">
-            <ul class="nav nav-tabs justify-content-center" id="myTab" role="tablist">
-                <li class="nav-item" role="presentation">
-                    <a href="javascript:void(0)" class="nav-link active" id="prominent-tab" data-toggle="tab"
-                       data-target="#prominent" role="tab" aria-controls="prominent" aria-selected="false">
-                        Prominent Headlines
-                    </a>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <a href="javascript:void(0)" class="nav-link" id="action-tab" data-toggle="tab" data-target="#action"
-                       role="tab" aria-controls="digital" aria-selected="false">
-                        Call to Action
-                    </a>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <a href="javascript:void(0)" class="nav-link" id="email-form-tab" data-toggle="tab" data-target="#email-form"
-                       role="tab" aria-controls="email-form" aria-selected="false">
-                        Email Signup Form
-                    </a>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <a href="javascript:void(0)" class="nav-link" id="visitual-content-tab" data-toggle="tab"
-                       data-target="#visitual-content" role="tab" aria-controls="visitual-content" aria-selected="false">
-                        Visual Content
-                    </a>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <a href="javascript:void(0)" class="nav-link" id="social-proof-tab" data-toggle="tab"
-                       data-target="#social-proof" role="tab" aria-controls="social-proof" aria-selected="true">
-                        Social Proof
-                    </a>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <a href="javascript:void(0)" class="nav-link" id="urgency-tab" data-toggle="tab"
-                       data-target="#urgency" role="tab" aria-controls="urgency" aria-selected="true">
-                        Urgency
-                    </a>
-                </li>
-            </ul>
-        </div>
-        <div class="tab-content" id="myTabContent">
-            <div class="tab-pane fade active show" id="prominent" role="tabpanel" aria-labelledby="prominent-tab">
+        <div class="mt-30">
+            <div class="mb-30">
                 <div class="h5 mt-30">Prominent Headlines</div>
                 <p>The main headline is placed on the top of the landing page with a relatively large font. It conveys the message with the depiction of the clear purpose behind creating the landing page.
                 </p>
             </div>
-            <div class="tab-pane fade" id="action" role="tabpanel" aria-labelledby="action-tab">
+            <div id="read-more-3" style="display: none;">
+              <div class="mb-30">
                 <div class="h5 mt-30">Call to Action</div>
                 <p>The call to action buttons such as "register now", "buy now", "join the community", "signup", "add to cart", "Register for the newsletter" should be easy to spot on the page. The use of the right contrast colors on the button would make it stand out in the content.</p>
                 <p>The ultimate goal of the landing page template is to increase the conversion. If none of the users reaches the page and no action is taken, then whole efforts are futile.</p>
-            </div>
-            <div class="tab-pane fade" id="email-form" role="tabpanel" aria-labelledby="email-form-tab">
+              </div>
+              <div class="mb-30">
                 <div class="h5 mt-30">Email Signup Form</div>
                 <p>A clean and neat email signup form makes the users fill it without any trouble. Too many widgets, extra fields, difficult drop and selection process would make your signup form too bloated with unwanted stuff. It would help if you focused on reducing the time and effort a person would take to fill the signup form. It should be quick and seamless without any error on the page.</p>
-            </div>
-            <div class="tab-pane fade" id="visitual-content" role="tabpanel" aria-labelledby="visitual-content-tab">
+              </div>
+              <div class="mb-30">
                 <div class="h5 mt-30">Visual Content</div>
                 <p>Enhance the user experience with digital media such as photos and videos. Arranging the digital media on the landing page template bootstrap is an art. You cannot simply put the images or videos anywhere on the page because they look good.</p>
                 <p>It would be best if you gave thought to how the user will scroll the page. You want them to stop and make them reach the call to action so that the user will fill the form, and the goal of the landing page will get complete.</p>
-            </div>
-            <div class="tab-pane fade" id="social-proof" role="tabpanel" aria-labelledby="social-proof-tab">
+              </div>
+              <div class="mb-30">
                 <div class="h5 mt-30">Social Proof</div>
                 <p>Use the past publication to show your presence in the industry. This template must have the trust-building factors that encourage people to signup for the service. Use your PR content to depict the social proof. Your visitor will feel safe about their provided information, and they will show trust in your brand.</p>
-            </div>
-            <div class="tab-pane fade" id="urgency" role="tabpanel" aria-labelledby="urgency-tab">
+              </div>
+              <div>
                 <div class="h5 mt-30">Urgency</div>
                 <p>The urgency is used to trigger the human emotion that makes the end-user feel they will lose something if they do not act immediately. Use certain words on the landing page, such as limited time offers, only 10 products left, early-bird discounts etc., to make people signup today.</p>
+              </div>
+            </div>
+            <div class="mt-45">
+              <a href="javascript:void(0)" class="btn btn-primary-gred read-more-link" data-href="#read-more-3">
+                Read More
+              </a>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section pt-0">
+    <div class="container">
+        <div class="highlight-box">
+            <div class="row no-gutters align-items-center flex-lg-row-reverse">
+                <div class="col-lg-8">
+                    <div class="highlight-content">
+                        <h3 class="mb-30">Drive More Customers With Landing Page Template</h3>
+                        <p>Bootstrap landing page themes build by the Lettstart Design are readily available to make live on your website. Bootstrap landing page download is quick. Download the theme, make it live on your server, make the desire changes, and your landing page is ready to fly. You can find a wide range of custom templates for landing page the Lettstart Design.</p>
+                        <p>The website has a premium quality template at an affordable price. Use the demo section to view the template in real-time. Every landing page is designed with modern technology and web development standards. More thought is put into improving the user experience and reduce the efforts to browse the information. It is highly converting landing pages found on the web.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <img src="{{ url('assets/images/lp-main.jpg') }}" class="img-fluid" alt=""/>
+                </div>
             </div>
         </div>
     </div>
 </section>
 <section class="section bg-light">
-    <div class="container">
-        <h3 class="mb-30">Drive More Customers With Landing Page Template</h3>
-        <p>Bootstrap landing page themes build by the Lettstart Design are readily available to make live on your website. Bootstrap landing page download is quick. Download the theme, make it live on your server, make the desire changes, and your landing page is ready to fly. You can find a wide range of custom templates for landing page the Lettstart Design.</p>
-        <p>The website has a premium quality template at an affordable price. Use the demo section to view the template in real-time. Every landing page is designed with modern technology and web development standards. More thought is put into improving the user experience and reduce the efforts to browse the information. It is highly converting landing pages found on the web.</p>
-    </div>
-</section>
-<section class="section">
     <div class="container">
         <h3 class="mb-30">Why Choose Lettstart Design?</h3>
         <p>We are following modern technology with the state of the art design to offer a pleasant experience to the user. We understand that it is essential if you want to win the heart of your customer. The bootstrap landing page template developed by Lettstart Design offers many benefits to the user.</p>
