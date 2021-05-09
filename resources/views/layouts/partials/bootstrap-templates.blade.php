65dc0b1e831ef30d6704d879b1d5edd2bc01c833
Gagan <ggbbhatia123@gmail.com>
2021-05-09 20:11:42 +0530

    bootstrap template changes

--- resources/views/layouts/partials/bootstrap-templates.blade.php
+++ resources/views/layouts/partials/bootstrap-templates.blade.php
@@ -1,20 +1,12 @@
 <section class="section bg-light">
   <div class="container">
-    <h2 class="mb-30">Why Choose Premium Bootstrap Templates for Website Development?</h2>
-    <p>Premium bootstrap templates are designed for easy use. It mitigates the compatibility issue on a wide range of
-      devices. Bootstrap technology allows the website to be responsive.</p>
-    <p>A single design is compatible with all the modern devices and screen sizes available in the markets.
-      Additionally, bootstrap enhances the user experience.</p>
+    <h2 class="mb-30">Why Choose Bootstrap Templates for Website Development?</h2>
+    <p>Bootstrap templates are designed for easy use. It mitigates the compatibility issue on a wide range of devices. Bootstrap technology allows the website to be flexible.</p>
+    <p>A single design is compatible with all the modern devices and screen sizes available in the markets. Additionally, bootstrap enhances the user experience.</p>
     <div id="read-more-1" style="display: none;">
-      <p>Speed, easy browsing, fast development, quick functional changes, freedom to customize code, and open-source
-        platforms are key highlights of the bootstrap template. Users get to enjoy several benefits while obtaining the
-        bootstrap technology.</p>
-      <p>Moreover, the future expansion of the website layout is possible without needing to change the base structure.
-        Small changes to the core files would be sufficient to apply the required changes to the whole website.</p>
-      <p>Due to the flexibility that bootstraps, many top companies trust the <a
-          href="https://lettstartdesign.com/category/bootstrap-templates">bootstrap templates</a> for their corporate
-        websites. The technology enables you to make a stellar website with the free bootstrap template. These templates
-        would give you exclusivity and make your website stand out in the crowd.</p>
+      <p>Speed, easy browsing, fast development, quick functional changes, freedom to customize code, and open-source platforms are key highlights of the bootstrap website templates. Users get to enjoy several benefits while obtaining the bootstrap technology.</p>
+      <p>Moreover, the future expansion of the website layout is possible without needing to change the base structure. Small changes to the core files would be sufficient to apply the required changes to the whole website.</p>
+      <p>Due to the flexibility that bootstraps, many top companies trust the bootstrap templates for their corporate websites. The technology enables you to make a stellar website with the free bootstrap templates. These templates would give you exclusivity and make your website stand out in the crowd.</p>
     </div>
     <div class="mt-30">
       <a href="javascript:void(0)" class="btn btn-primary-gred read-more-link" data-href="#read-more-1">
@@ -25,47 +17,45 @@
 </section>
 <section class="section">
   <div class="container">
-    <div class="row flex-lg-row-reverse align-items-center">
-      <div class="col-lg-8 pl-lg-5">
-        <h3 class="mb-30">Amplify Scalability and Compatibility</h3>
-        <p>Bootstrap themes promote mobile-first technology. The single website would be compatible with desktop, mobile
-          devices, and tablets. Use premium bootstrap templates for building your website with ease.</p>
-        <p>Install the theme on the server, and the site is live for the users. If you are not willing to spend money
-          upfront, you can select free premium bootstrap themes. These themes are offered free of cost with no
-          restraint.</p>
-        <p>The scalability of the existing digital infrastructure is a daunting task. The bootstrap simplifies the
-          process with the help of the futuristic coding framework that facilitates the programmer to extend the number
-          of pages or features without needing to change the site's core functionality.</p>
+    <h3 class="mb-30">How to Start the Website With the Use of a Free Bootstrap Templates?</h3>
+    <p>With the use of the free bootstrap website templates, the opportunities are endless. It is one of the most used HTML, CSS, and JS frameworks in the world. When it comes to front-end development, bootstrap is a favorite framework among front-end web developers.</p>
+    <p>It allows the developer to build a high-quality website with ease. Quickly construct the codes, and your design will be ready to go live on all the available platforms and modern devices.</p>
+    <div id="read-more-2" style="display: none;">
+      <p>Less complexity and fast turnaround time make the bootstrap framework the first choice for developing the website template.</p>
+      <p>Do not have resources or funds to build the website? No worries. Get the free or premium bootstrap template from the LettStart Design that comes with the ready-to-use capability. Pay the amount and download the template to your desktop.</p>
+      <p>The downloaded bootstrap template comes with all the required resources, files, scripts, images, and CSS files, making your website live immediately. Upload the theme on your hosting server, and your website will be active.</p>
+      <p>All the templates are tested for quality and compatibility on various devices. Unique design makes your website stand out and generates more revenue for your business.</p>
+      <p>More focus is given on the small aspects of template design that make a big difference in engaging the users. The objectives of the business define each user's journey. The elegant design of the template will help you to convert your users into customers.</p>
+      <div class="mt-30">
+        <div class="mb-30">
+          <div class="h5 mt-30">Amplify Scalability and Compatibility</div>
+          <p>Bootstrap themes promote mobile-first technology. The single website would be compatible with desktop, mobile devices, and tablets. Use premium bootstrap templates for building your website with ease.</p>
+        <p>Install the theme on the server, and the site is live for the users. If you are not willing to spend money upfront, you can select free premium bootstrap themes. These themes are offered free of cost with no restraint.</p>
+        <p>The scalability of the existing digital infrastructure is a daunting task. The bootstrap simplifies the process with the help of the futuristic coding framework that facilitates the programmer to extend the number of pages or features without needing to change the site's core functionality.</p>
+        </div>
       </div>
-      <div class="col-lg-4">
-        <img src="{{ url('assets/images/bootstrap-main.png') }}" class="img-fluid" alt="premium bootstrap templates" />
-      </div>
+    </div>
+    <div class="mt-30">
+      <a href="javascript:void(0)" class="btn btn-primary-gred read-more-link" data-href="#read-more-2">
+        Read More
+      </a>
     </div>
   </div>
 </section>
 <section class="section bg-light">
   <div class="container">
-    <h3 class="mb-30">Benefits of Using Premium Bootstrap Templates</h3>
-    <p>This template come with more features and customization functionality. Compared with the free template, they come
-      with limited resources and the technology used in the development of the themes.</p>
-    <p>You get premium support when you buy the premium bootstrap theme. Most importantly, the free templates are widely
-      used, which takes away the exclusivity factor of the website design.</p>
+    <h3 class="mb-30">Benefits of Using Bootstrap Website Templates</h3>
+    <p>This template come with more features and customization functionality. Compared with the free template, they come with limited resources and the technology used in the development of the themes.</p>
+    <p>You get premium support when you buy the premium bootstrap theme. Most importantly, the free templates are widely used, which takes away the exclusivity factor of the website design. </p>
     <div class="mt-30">
       <div class="mb-30">
         <div class="h5 mt-30">Easy to Use</div>
-        <p>Bootstrap templates are easy to use. No special skill requires installing the theme on the hosting server.
-          The premium theme comes with all the required resources, so you do not have to add or remove anything from the
-          template. It is a robust template design to engage the users.</p>
+        <p>Bootstrap blog or website templates are easy to use. No special skill requires installing the theme on the hosting server. The premium theme comes with all the required resources, so you do not have to add or remove anything from the template. It is a robust template design to engage the users.</p>
       </div>
       <div id="read-more-3" style="display: none;">
         <div class="mb-30">
-          <div class="h5 mt-30">Call to Action</div>
-          <p>The call to action buttons such as "register now", "buy now", "join the community", "signup", "add to cart", "Register for the newsletter" should be easy to spot on the page. The use of the right contrast colors on the button would make it stand out in the content.</p>
-          <p>The ultimate goal of the landing page template is to increase the conversion. If none of the users reaches the page and no action is taken, then whole efforts are futile.</p>
-        </div>
-        <div class="mb-30">
             <div class="h5 mt-30">Auto Adjust to the screen size</div>
-            <p>With the premium bootstrap templates, you do not have to worry about the browser or the device size of the end-users. The templates come with auto-adjusted and modification features that align the element precisely to offer a great experience to the user. All the themes developed by the LettStart Design fit perfectly to computer, mobile, and tablet screens. </p>
+            <p>With the bootstrap templates, you do not have to worry about the browser or the device size of the end-users. The templates come with auto-adjusted and modification features that align the element precisely to offer a great experience to the user. All the themes developed by the LettStart Design fit perfectly to computer, mobile, and tablet screens.</p>
         </div>
         <div class="mb-30">
             <div class="h5 mt-30">Customization</div>
@@ -79,7 +69,11 @@
         </div>
         <div>
             <div class="h5 mt-30">Cost-Effective</div>
-            <p><a href="https://lettstartdesign.com/">Bootstrap themes</a> come at affordable pricing. Comparing to the other technology, the bootstrap themes are required low investment. Also, you do not need a web designer or programmer to make changes to the site. So there will be no cost after you purchased the themes. A layperson with a basic understanding of HTML and CSS can change the template. It would be wise to buy the premium theme instead of wasting your time building the website from nothing.</p>
+            <p><a href="https://lettstartdesign.com/">Bootstrap themes</a> come at affordable pricing. Comparing to the other technology, the bootstrap themes are required low investment. Also, you do not need a web designer or programmer to make changes to the site. So there will be no cost after you purchased the themes. A layperson with a basic understanding of HTML and CSS can change the template. It would be wise to buy the premium theme instead of wasting your time building the website from nothing. </p>
+        </div>
+        <div>
+            <div class="h5 mt-30">Easy Integration</div>
+            <p>Bootstrap templates are useful when upgrading the existing website template or adding new pages to the existing sites. You can easily connect the new page with the exciting theme without needing much effort. For the beginner programmer who does not have much experience in template integration, bootstrap could be an easy solution. You can make the page live for emergency use and work on the technical aspect of the page later. It serves the purpose and offers instant access to web content.</p>
         </div>
       </div>
       <div class="mt-45">
@@ -92,44 +86,21 @@
 </section>
 <section class="section">
   <div class="container">
-    <h3 class="mb-30">Drive More Customers With Landing Page Template</h3>
-    <p>Bootstrap landing page themes build by the Lettstart Design are readily available to make live on your website.
-      Bootstrap landing page download is quick. Download the theme, make it live on your server, make the desire
-      changes, and your landing page is ready to fly. You can find a wide range of custom templates for landing page the
-      Lettstart Design.</p>
-    <p>The website has a premium quality template at an affordable price. Use the demo section to view the template in
-      real-time. Every landing page is designed with modern technology and web development standards. More thought is
-      put into improving the user experience and reduce the efforts to browse the information. It is highly converting
-      landing pages found on the web.</p>
+    
+    <div class="row flex-lg-row-reverse align-items-center">
+      <div class="col-lg-8 pl-lg-5">
+        <h3 class="mb-30">Why Choose the LettStart Design?</h3>
+        <p>LettStart Design is a growing bootstrap resource development platform to find several free and premium templates designed by an experienced developer. The ready-to-customize bootstrap themes are made with advanced coding practices, frameworks, neat design patterns, and easy-to-publish methods.</p>
+        <p>Download your favorite template from the LettStart Design, upload it on your server, make the content changes, and your business is ready to fly. You get a complete website with essential pages developed to match modern devices and web browsers.</p>
+       <p>No need to spend your time in long hours programming. Use the ready-to-use LettStart Design template and start your business instantly. Use the premium admin template bootstrap for content management.</p>
+      </div>
+      <div class="col-lg-4">
+        <img src="{{ url('assets/images/bootstrap-main.png') }}" class="img-fluid" alt="premium bootstrap templates" />
+      </div>
+    </div>
   </div>
 </section>
 <section class="section bg-light">
-  <div class="container">
-    <h3 class="mb-30">Why Choose Lettstart Design?</h3>
-    <p>We are following modern technology with the state of the art design to offer a pleasant experience to the user.
-      We understand that it is essential if you want to win the heart of your customer. The bootstrap landing page
-      template developed by Lettstart Design offers many benefits to the user.</p>
-    <div id="read-more-2" style="display: none;">
-      <p>Several features are geared into the single page to reduce the efforts and time spent accessing the landing
-        page. The conversion rate of your marketing campaign would skyrocket when you start using the landing page
-        template bootstrap.</p>
-      <p>Browse from the hundreds of high-quality bootstrap landing page template listed on the Lettstart Design. Get
-        ready to use a template and start building your reputation on the web. For any query, you can reach the customer
-        support team of Lettstart Design.</p>
-      <p>The team will help you to select the right template suitable for your need. With 1-year customer support as
-        part of the package, you can implement the design without worrying about the technical aspect of the design.
-        Whenever you get stuck, contact the customer support team, and the problem will be resolved as quickly as
-        possible.
-      </p>
-    </div>
-    <div class="mt-45">
-      <a href="javascript:void(0)" class="btn btn-primary-gred read-more-link" data-href="#read-more-2">
-        Read More
-      </a>
-    </div>
-  </div>
-</section>
-<section class="section">
   <div class="container">
     <h3 class="mb-30">Frequently Asked Questions</h3>
     <div class="accordion" id="accordionExample">
