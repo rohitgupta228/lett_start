5057cad7db1b2e1d0db7374c489b81779895e92e
Gagan <ggbbhatia123@gmail.com>
2021-05-11 22:46:32 +0530

    business coporate content

---
+++ resources/views/layouts/partials/business-corporate-templates.blade.php
@@ -0,0 +1,170 @@
+<section class="section bg-light">
+  <div class="container">
+    <h2 class="mb-30">Bootstrap Business & Corporate Templates Download</h2>
+    <p>Online presence is vital for any business today. Having a corporate website with the right information on the web page would attract more customers. It allows the users to connect with your brand and encourage them to take buying decision.</p>
+    <p>The design of the website makes a big difference in shifting the attention of the end-users. The impeccable corporate and business bootstrap templates would enhance the user experience.</p>
+    <div id="read-more-1" style="display: none;">
+      <p>Going digital was never so easy before. With the bootstrap template for business website, anyone can start their online business instantly. A wide range of free and premium template options are available to download.</p>
+      <p>Get your favorite design downloaded on your pc, upload on your server, make the required content changes, and your business is ready to fly. Bootstrap business & corporate templates collection offers various choices in the selection process.</p>
+      <p>Full customization is possible in the bootstrap template. Build with the easy navigation, functionally rich, content management, dynamic page construction, responsive layout and compatible to all the modern browsers are some of the features you get to enjoy with the bootstrap template.</p>
+    </div>
+    <div class="mt-30">
+      <a href="javascript:void(0)" class="btn btn-primary-gred read-more-link" data-href="#read-more-1">
+        Read More
+      </a>
+    </div>
+  </div>
+</section>
+<section class="section">
+  <div class="container">
+    <h3 class="mb-30">Why Should You Choose the Corporate and Business Template?</h3>
+    <p>When it comes to global exposure of your brand, the website is the first thing that your customers will observe. Your first impression of the customer is completely reliant on the type of website you have. As a brand, you must choose the right technology to depict your strength, quality, and identity in the global market.</p>
+    <p>Bootstrap is one of modern businesses' HTML and CSS frameworks to build corporate websites. The Lettstart Design has come with the best bootstrap templates for business that are ready to use.</p>
+    <div id="read-more-2" style="display: none;">
+      <p>Choose the preferred design and download the template on your desktop with the license. Once you have the template in hand, upload it on your server, and your website will be active. You can make the content changes ongoing meanwhile work on the functional changes to meet the business requirements.</p>
+      <p>Bootstrap corporate theme is made of a framework that is easy to code. You can add as many elements as you want, update the exiting color of the theme, connect the third-party plug-in, and enhance the quality of the website. Bootstrap offers you complete flexibilities in doing what you want without needing to spend more time on the customization.</p>
+    </div>
+    <div class="mt-30">
+      <a href="javascript:void(0)" class="btn btn-primary-gred read-more-link" data-href="#read-more-2">
+        Read More
+      </a>
+    </div>
+  </div>
+</section>
+<section class="section bg-light">
+  <div class="container">
+    <h3 class="mb-30">Benefits of the Corporate Business Bootstrap Template Download</h3>
+    <div>
+      <div class="h5 mt-30">Responsive</div>
+      <p>One of the great deals with the modern website template is the responsiveness of the design. It should be device-friendly and easily adaptable to the screen sizes. The bootstrap corporate theme comes with responsive technology. All the bootstrap templates are built using mobile and device responsive technology. So you do not have to make changes separately. Download the template and make it live. It will automatically adjust itself according to the screen size.</p>
+    </div>
+    <div class="mt-30">
+      <div class="mb-30">
+        <div class="h5 mt-30">No Need for Coding Experience</div>
+        <p>All the bootstrap corporate templates are made of fully functional features. The necessary elements are placed in the right order to offer a great experience while using the website. A user-friendly interface and a simple to use control panel makes the bootstrap the developer's first choice. Anyone can make the bootstrap business templates live and start generating revenue. No coding is needed. Change the content of the site according to your business requirements, and you are ready to go.</p>
+      </div>
+      <div id="read-more-3" style="display: none;">
+        <div class="mb-30">
+            <div class="h5 mt-30">Save Time and Money</div>
+            <p>Building a responsive website is a daunting task. If you decide to hire the developer or design agency to make the illustration, it may take them several months to build the complete mobile responsive website.</p>
+            <p>It is not a viable solution for the small-scale industries where the market changes overnight, and the business that was booming a few days back may now do not have the demand. Therefore, the startups and small businesses trying to capture the large market share need an instant solution.</p>
+            <p>Modern business start bootstrap templates with ease and make their website live immediately once they have a vision for the site. Choose the template from the list offered by the Lettstart Design. Pay the amount to purchase the license, and the template will be yours. Ready to use templates saves money and time. It enables businesses to focus on other important aspects.</p>
+        </div>
+        <div class="mb-30">
+            <div class="h5 mt-30">Key Features of the Bootstrap Business Templates Listed on the Lettstart Design</div>
+            <div class="checklist">
+              <ul class="list-unstyled">
+                <li>
+                  <i class="bx bxs-check-circle text-success h5 mb-0 mr-2 font-weight-normal"></i>
+                  <span>Easy customization and ready to use</span>
+                </li>
+                <li>
+                  <i class="bx bxs-check-circle text-success h5 mb-0 mr-2 font-weight-normal"></i>
+                  <span>Mobile, Desktop, and Tablet compatible designs</span>
+                </li>
+                <li>
+                  <i class="bx bxs-check-circle text-success h5 mb-0 mr-2 font-weight-normal"></i>
+                  <span>Combination of blogs and pages for easy update</span>
+                </li>
+                <li>
+                  <i class="bx bxs-check-circle text-success h5 mb-0 mr-2 font-weight-normal"></i>
+                  <span>Integration of the third party application</span>
+                </li>
+                <li>
+                  <i class="bx bxs-check-circle text-success h5 mb-0 mr-2 font-weight-normal"></i>
+                  <span>Inbuilt working Contact form</span>
+                </li>
+                <li>
+                  <i class="bx bxs-check-circle text-success h5 mb-0 mr-2 font-weight-normal"></i>
+                  <span>Code validation as the W3C standards</span>
+                </li>
+                <li>
+                  <i class="bx bxs-check-circle text-success h5 mb-0 mr-2 font-weight-normal"></i>
+                  <span>1-year premium support</span>
+                </li>
+                <li>
+                  <i class="bx bxs-check-circle text-success h5 mb-0 mr-2 font-weight-normal"></i>
+                  <span>Detailed documentation with each premium template download</span>
+                </li>
+                <li>
+                  <i class="bx bxs-check-circle text-success h5 mb-0 mr-2 font-weight-normal"></i>
+                  <span>Built-in latest bootstrap 4 technology</span>
+                </li>
+                <li>
+                  <i class="bx bxs-check-circle text-success h5 mb-0 mr-2 font-weight-normal"></i>
+                  <span>All browser compatible</span>
+                </li>
+                <li>
+                  <i class="bx bxs-check-circle text-success h5 mb-0 mr-2 font-weight-normal"></i>
+                  <span>Option to choose a wide range of colors for your template</span>
+                </li>
+                <li>
+                  <i class="bx bxs-check-circle text-success h5 mb-0 mr-2 font-weight-normal"></i>
+                  <span>Free Lifetime update</span>
+                </li>
+                <li>
+                  <i class="bx bxs-check-circle text-success h5 mb-0 mr-2 font-weight-normal"></i>
+                  <span>Attractive animation</span>
+                </li>
+              </ul>
+          </div>
+        </div>
+      </div>
+      <div class="mt-45">
+        <a href="javascript:void(0)" class="btn btn-primary-gred read-more-link" data-href="#read-more-3">
+          Read More
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="section">
+  <div class="container">
+    <h3 class="mb-30">Where Can I Download Bootstrap Corporate Templates Free?</h3>
+    <p>Lettstart Design offers various bootstrap corporate templates for free download. These ready-to-use templates come with advanced features. You can download them from the Lettstart Design website and upload them on your server to make the site active.</p>
+    <p>However, before you decide to use the template, read the guidelines carefully. The free bootstrap corporate templates come with user guidelines. Unlike premium corporate templates, you will not benefit from the 1-year support from the Lettstart Design. </p>
+   <p>Limited support is offered to the users who choose to go with the free bootstrap template option. Also, you cannot resell these templates on your portal. Free corporate templates come with copyright restrictions limiting users from selling the template made by Lettstart Design on the other platforms.</p>
+  </div>
+</section>
+<section class="section pt-0">
+    <div class="container">
+        <div class="highlight-box">
+            <div class="row no-gutters align-items-center flex-lg-row-reverse">
+                <div class="col-lg-8">
+                    <div class="highlight-content">
+                      <h3 class="mb-30">Conclusion</h3>
+                      <p>If you search for the best bootstrap templates for business or bootstrap corporate theme, your search ends here. The Lettstart Design has everything that you need. We have an extensive collection of bootstrap templates. You can download the premium and free templates from the website that suits your need. </p>
+                      <p>The customization option would give you the flexibility to add or remove elements from the design. Make the required functional changes. Connect more plug-in in the templates to make some of the pages dynamic and improve the site's quality. Get bootstrap premium theme today and start your web journey.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                  <img src="{{ url('assets/images/business-corporate.jpg') }}" class="img-fluid" alt="premium bootstrap templates" />
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section">
+  <div class="container">
+    <h3 class="mb-30">Frequently Asked Questions</h3>
+    <div class="accordion" id="accordionExample">
+      @foreach($faqList[$category] as $faq)
+
+      <div class="card active">
+        <div class="card-header" id="heading{{ $loop->index }}">
+          <a href="javascript:void(0)" class="accordion-toggle text-left" data-toggle="collapse"
+            data-target="#collapse{{ $loop->index }}" aria-expanded="true" aria-controls="collapse{{ $loop->index }}">
+            {!! $faq['question'] !!}
+          </a>
+        </div>
+
+        <div id="collapse{{ $loop->index }}" class="collapse show" aria-labelledby="heading{{ $loop->index }}">
+          <div class="card-body">
+            {!! $faq['answer'] !!}
+          </div>
+        </div>
+      </div>
+      @endforeach
+    </div>
+  </div>
+</section>
